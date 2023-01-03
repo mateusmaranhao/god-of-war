75ef6a39abf3325ffc6ada142ac2ecadf014ba5f
Mateus Maranhão <mateusmaranhao0210@gmail.com>
2023-01-03 19:38:40 -0300

    adicionando responsividade ao slide da s-character

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@ var swiper = new Swiper(".slide-character", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+      },
+      768: {
+        slidesPerView: 2.2,
+      },
+      991: {
+        slidesPerView: 2.8,
+      },
+      1200: {
+        slidesPerView: 3.5,
+      }
+    },
   });
